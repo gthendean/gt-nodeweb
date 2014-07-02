ffb11405604840be7128f2b375602a6c0c94e237
Gabriel Thendean <gabriel.thendean@sap.com>
2014-07-02 00:25:08 -0700

    Change the page content
    Change-Id: Ibc9c6427f6af6e4e884fb91d5f9f3e944623e9c0

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello Azure with Node.js\n');
 }).listen(port);
 console.log('Listening to port ' + port + '...');
